eaf2ff9eba127ae7f96a4afd0e0754330a8a2ab7
ananda2408 <ananda.camargo.silva@escola.pr.gov.br>
2025-04-10 09:25:33 -0300

    Create compara_strings.py

---
+++ compara_strings.py
@@ -0,0 +1,9 @@
+# Solicita ao usuário que insira duas letras
+letra1 = input("Digite o 1o. letra:  ")
+letra2 = input("Digite o 2o. letra: ")
+
+# Verifica comparando se as letras são iguais
+if letra1 == letra2:
+    print("as letras são iguais.")
+else:
+    print("as letras são diferentes.")
