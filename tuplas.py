a3324ea707a05e028a60729a5bf39ad4f4958cab
ananda2408 <ananda.camargo.silva@escola.pr.gov.br>
2025-05-20 08:43:53 -0300

    Create tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,9 @@
+# Definição implícita de uma tupla
+tupla_implicita = (1, 2, 3, 4)
+
+# Definição explícita de uma tupla
+lista = [5, 6, 7, 8]
+tupla_explicita = tuple(lista)
+
+print("Tupla implícita:", tupla_implicita, "| Tipo:", type(tupla_implicita))
+print("Tupla explícita:", tupla_explicita, "| Tipo:", type(tupla_explicita))
